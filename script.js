648061aee6c3ec19440e934d22d63570bca1e1f3
OduardoRuiz <carloseduardoruizs@gmail.com>
2023-04-18 11:28:46 -0300

    corrigindo botão baixar CV

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function downloadCvEnglish() {
     const link = document.createElement('a');
-    link.href = '/OduardoRuiz/eduardoruiz/raw/main/document/cvEnglish.pdf';
+    link.href = 'https://github.com/OduardoRuiz/eduardoruiz/raw/main/document/cvEnglish.pdf';
     link.download = '01 - Eduardo Ruiz - English.pdf';
     link.dispatchEvent(new MouseEvent('click'));
   }
   function downloadCvPortugues() {
     const link = document.createElement('a');
-    link.href = '/OduardoRuiz/eduardoruiz/raw/main/document/cvPortuguesN.pdf';
+    link.href = 'https://github.com/OduardoRuiz/eduardoruiz/raw/main/document/cvPortuguesN.pdf';
     link.download = '01 - Eduardo Ruiz - Português.pdf';
     link.dispatchEvent(new MouseEvent('click'));
   }
