d784e4f075ef7a4eda2bac7f6323477919f30209
OduardoRuiz <carloseduardoruizs@gmail.com>
2023-04-18 18:13:47 -0300

    estilização footer + tela projetos

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+const script = document.createElement('script');
+script.src = 'https://kit.fontawesome.com/5819207453.js';
+script.crossOrigin = 'anonymous';
+document.body.appendChild(script);
+
+
 function downloadCvEnglish() {
-    const link = document.createElement('a');
-    link.href = './document/cvEnglish.pdf';
-    link.download = '01 - Eduardo Ruiz - English.pdf';
-    link.dispatchEvent(new MouseEvent('click'));
-  }
-  function downloadCvPortugues() {
-    const link = document.createElement('a');
-    link.href = './document/cvPortuguesN.pdf';
-    link.download = '01 - Eduardo Ruiz - Português.pdf';
-    link.dispatchEvent(new MouseEvent('click'));
-  }
-  
-  
+  const link = document.createElement('a');
+  link.href = './document/cvEnglish.pdf';
+  link.download = '01 - Eduardo Ruiz - English.pdf';
+  link.dispatchEvent(new MouseEvent('click'));
+}
+function downloadCvPortugues() {
+  const link = document.createElement('a');
+  link.href = './document/cvPortuguesN.pdf';
+  link.download = '01 - Eduardo Ruiz - Português.pdf';
+  link.dispatchEvent(new MouseEvent('click'));
+}
+
+
