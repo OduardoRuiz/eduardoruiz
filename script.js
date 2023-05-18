41442d3ae8d3c8fcd555bf49dca86b1c490a5926
Eduardo Ruiz <59852595+OduardoRuiz@users.noreply.github.com>
2023-05-18 15:40:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ document.body.appendChild(script);
 
 function downloadCvEnglish() {
   const link = document.createElement('a');
-  link.href = './document/CVEnglish.pdf';
+  link.href = './document/CVENGLISH.pdf';
   link.download = '01 - Eduardo Ruiz - English.pdf';
   link.dispatchEvent(new MouseEvent('click'));
 }
 function downloadCvPortugues() {
   const link = document.createElement('a');
-  link.href = './document/CVPortugues.pdf';
+  link.href = './document/CVPORTUGUES.pdf';
   link.download = '01 - Eduardo Ruiz - Português.pdf';
   link.dispatchEvent(new MouseEvent('click'));
 }
