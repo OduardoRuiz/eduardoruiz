879ef74fce73ed586b290587a231a18e63a5fca9
OduardoRuiz <carloseduardoruizs@gmail.com>
2024-09-11 23:05:56 -0300

    Atualização CV download

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ document.body.appendChild(script);
 
 function downloadCvEnglish() {
   const link = document.createElement('a');
-  link.href = './document/CVENGLISH.pdf';
+  link.href = './document/CVENGLISH1.pdf';
   link.download = '01 - Eduardo Ruiz - English.pdf';
   link.dispatchEvent(new MouseEvent('click'));
 }
 function downloadCvPortugues() {
   const link = document.createElement('a');
-  link.href = './document/CVPORTUGUES.pdf';
+  link.href = './document/CVPORTUGUES1.pdf';
   link.download = '01 - Eduardo Ruiz - Português.pdf';
   link.dispatchEvent(new MouseEvent('click'));
 }
